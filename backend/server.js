1a3fce4b15f41a673bd12c55ebb5e7f46457da4e
it21156960 <chamith227@gmail.com>
2024-01-02 22:39:52 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express =require('express')
-
+const cors=require('cors')
 
 //express app
 const app=express()
@@ -23,11 +23,11 @@ app.use((req,res,next)=>{
 MongoClient.connect(uri)
 .then(client=>{
     console.log("connected to database");
-    app.listen(PORT,()=>console.log("server is listening port"));
+     
 
     const db = client.db('foodrush');
 
-    const restaurant=db.collection('foodrush')
+    const restaurant=db.collection('restaurant')
 
     app.use(cors())
     app.use(express.json())
@@ -36,8 +36,77 @@ MongoClient.connect(uri)
         console.log('server is running on port', PORT)
     })   
 
+    app.post('/addrestaurant',(req,res)=>{
+
+        restaurant.insertOne(req.body)
+        .then(result=>{
+            res.status(200).json({message:"success"})
+        })
+    .catch(error=>{
+       res.status(500).json({
+        sucess:false
+       })
+    })
 })
+
+    app.get('/getrestaurant',(req,res)=>{
+        restaurant.find({})
+        .toArray()
+        .then(results=>{
+            res.status(200).json({
+                sucess:true,
+                data:results
+            });
+        }).catch(err=>{
+            res.status(500),json({
+                sucess:false,
+
+            })
+        })
+    })
+
+    app.put('/updaterestaurant/:id', (req, res) => {
+        const restaurantId = req.params.id;
+    
+        restaurant.findOneAndUpdate(
+            { _id: restaurantId },
+            {
+                $set: req.body,
+                
+            },
+           
+        ).then(updatedRestaurant => {
+            res.json({
+                success: true,
+                updatedRestaurant: updatedRestaurant
+            });
+        }).catch(err => {
+            res.status(500).json({
+                success: false,
+                error: err.message
+            });
+        });
+    });
+    
+
+    app.delete('/deleteRestaurant/:id',(req,res)=>{
+        restaurant.findOneAndDelete({
+            _id: req.params.id
+        }).then(result=>{
+            res.status(200).json({
+                sucess:true,
+                message:"deleted"
+            })
+        }).catch(err=>{
+            res.status(500).json({
+                sucess:false,
+            })
+        })
+
 .catch(error=>{ 
     console.log(error)
+}) 
+
+    })
 })
 
