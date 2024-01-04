fc32b1dd2afb607d9cd1639b3456439b84f92a4c
it21156960 <chamith227@gmail.com>
2024-01-04 15:18:33 +0530

    restaurant_route_added

--- backend/routes/restaurant.js
+++ backend/routes/restaurant.js
@@ -1,6 +1,10 @@
-const express=require('express')
+const express = require('express');
+const router = express.Router();
+const restaurantController = require('../controllers/restaurantCtrl');
 
-const router=express.Router()
+router.post('/addrestaurant', restaurantController.addRestaurant);
+router.get('/getrestaurant', restaurantController.getRestaurant);
+router.put('/updaterestaurant/:id', restaurantController.updateRestaurant);
+router.delete('/deleteRestaurant/:id', restaurantController.deleteRestaurant);
 
-
-module.exports=router 
+module.exports = router;
