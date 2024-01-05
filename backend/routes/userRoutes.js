458dac5a2b0aeb2ecc418dd6186eba9f21ea3cd2
it21156960 <chamith227@gmail.com>
2024-01-05 16:34:00 +0530

    UserRouteUpdated

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userCtrl');
+
+router.post('/adduser', userController.addUser);
+router.get('/getuser', userController.getUser);
+router.put('/updateuser/:id', userController.updateUser);
+router.delete('/deleteuser/:id', userController.deleteUser);
+
+module.exports = router;
