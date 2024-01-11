4a14669c9376fa808f9bfe5ca3f9903a30b39161
it21156960 <chamith227@gmail.com>
2024-01-12 00:02:27 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,12 @@ const restaurantRoute = require('./routes/restaurant');
 const userRoute = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; 
 
 
 app.use(cors());
 app.use(express.json());
+
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
