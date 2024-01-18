9e8efda0139d92f9d62e9467a488516842f67eb4
it21156960 <chamith227@gmail.com>
2024-01-19 01:48:52 +0530

    appCompoUpdated

--- frontend/my-app/src/app/app.component.ts
+++ frontend/my-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonService } from './Service/common.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  location:any;
+  constructor(private commonService:CommonService){
+
+  }
+  ngOnInit(): void {
+      this.commonService.getLocation().subscribe((data)=>{
+        console.log(data);
+        this.location=data;
+      })
+  }
 }
